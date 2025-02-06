e1ed035faaea612eee5cd967f4d10463f4fa8dd6
Dawid Nowotny <dawidnowotny@o2.pl>
2025-02-06 17:41:24 +0100

    Added functions to start communication websocket

--- backend/src/routers/node_router.py
+++ backend/src/routers/node_router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, UploadFile, Form, status, HTTPException
 
 from src.services.torrent_service import validate_pdf_request
 from src.services.pdf_service import validate_pdf
+from src.services.node_service import start_communication_websocket, close_communication_websocket
 from src.services.torrent_service import transfer_file
 from src.domain.TorrentNode import TorrentNode
 from src.constants import NODE_PORTS
@@ -24,8 +25,12 @@ async def send_pdf_to_node(
     polynomial: str = Form(...)
 ):
     global network
+
     node_request = validate_pdf_request(first_seeder, target_node, polynomial)
-    print(node_request.polynomial)
+    await validate_pdf(file)
+
+    file.file.seek(0)
+    start_communication_websocket()
 
     try:
         await transfer_file(file, node_request.first_seeder, node_request.target_node, network)
@@ -33,4 +38,5 @@ async def send_pdf_to_node(
     except HTTPException as e:
         raise e
     except Exception as e:
-        raise HTTPException(status_code=500, detail="Internal server error")
+        print(e)
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal server error")
